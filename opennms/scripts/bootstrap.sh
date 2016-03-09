53f368936e81b0a40be781d73c8730a6fc27932a
Jesse White <jesse@opennms.org>
2016-03-08 22:13:55 -0500

    Wait for Postgres to start.

--- opennms/scripts/bootstrap.sh
+++ opennms/scripts/bootstrap.sh
@@ -13,10 +13,19 @@ sed -i s/sshHost.*/sshHost=0.0.0.0/g "${OPENNMS_HOME}/etc/org.apache.karaf.shell
 # Expose ActiveMQ
 sed -i s/127.0.0.1:61616/0.0.0.0:61616/g "${OPENNMS_HOME}/etc/opennms-activemq.xml"
 
+echo "Waiting for Postgres to start..."
+WAIT=0
+while ! $(timeout 1 bash -c 'cat < /dev/null > /dev/tcp/$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_PORT_5432_TCP_PORT'); do
+  sleep 1
+  WAIT=$(($WAIT + 1))
+  if [ "$WAIT" -gt 15 ]; then
+    echo "Error: Timeout wating for Postgres to start"
+    exit 1
+  fi
+done
+
 # Start OpenNMS
 rm -rf ${OPENNMS_HOME}/data
 ${OPENNMS_HOME}/bin/runjava -s
 ${OPENNMS_HOME}/bin/install -dis
 "${OPENNMS_HOME}/bin/opennms" -f start
-
-sleep 60000
